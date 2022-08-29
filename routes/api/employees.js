1f0e7462fdab0317c5bd6a757db7901d254dc89a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-29 08:40:52 -0400

    initial commit of server

---
+++ routes/api/employees.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const data = {};
+data.employees = require('../../model/employees.json');
+const controller = require('../../controllers/EmployeeController')
+router
+  .route('/')
+  .get(controller.getAllEmployees)
+
+  .post(controller.createEmployee)
+  .put(controller.updateEmployee)
+  .delete(controller.deleteEmployee);
+
+router.route('/:id').get(controller.getEmployeeById);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = [
+  'https://create-react-app-form.vercel.app',
+  'http://localhost:3500',
+  'http://localhost:3000',
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+          console.log(origin)
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
